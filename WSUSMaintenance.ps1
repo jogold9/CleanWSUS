edb2452c02a49844345a241b14ea424101df8950
Josh <joshuabgold@gmail.com>
2019-03-22 12:39:36 -0700

    Added line to force Powershell best practices
    Added line to force Powershell best practices:  Set-StrictMode -Version Latest

--- WSUSMaintenance.ps1
+++ WSUSMaintenance.ps1
@@ -3,7 +3,13 @@
           # It is important to automate this using scheduled tasks as the WSUS database can quickly get too large to be functional.
 
           #If you are running this script locally, you do not need to specify anything, but if you need to run it against a remote WSUS server, 
-          # then specifying the –Name and –Port parameter will be required.
+          # then specifying the â€“Name and â€“Port parameter will be required.
+          
+          #Use these two lines if you want to force the script to follow Powershell best practices
+          Set-StrictMode -Version Latest
+
+          #Use this line if you want to push through any errors without screen output
+          #$ErrorActionPreference = "SilentlyContinue"
 
           Get-WSUSServer | Invoke-WsusServerCleanup -CleanupObsoleteUpdates -CleanupUnneededContentFiles -CompressUpdates -DeclineExpiredUpdates  -DeclineSupersededUpdates |
            Out-file -FilePath C:\WSUS\cleanup_results.txt
